f3254cda283017024e8c666b8433d87c824ccc66
lyctea <396136337@qq.com>
2018-12-19 18:13:56 +0800

    feat: 重构为async

--- dytt8/index.js
+++ dytt8/index.js
@@ -1,87 +1,96 @@
-var cheerio = require('cheerio');
-var http = require('http');
-var iconv = require('iconv-lite');
+const cheerio = require('cheerio');
+const http = require('http');
+const iconv = require('iconv-lite');
 const MongoClient = require('mongodb').MongoClient;
 
-var index = 1;
-var count = 1;
-const url = 'http://www.ygdy8.net/html/gndy/dyzz/list_23_';
-var titles = [];
-var btLink = [];
-
-function getTitle (url, i) {
-  console.log('正在获取底' + i + '页内容');
-  http.get(`${url}${i}.html`, function (res) {
-    var chunks = [];
-    res.on('data', function (chunk) {
-      chunks.push(chunk);
-    }).on('end', function () {
-      var html = iconv.decode(Buffer.concat(chunks), 'gb2312');
-      var $ = cheerio.load(html, {decodeEntities: false});
+function getMoveHomePage (url, i) {
+  console.log('正在获取: ' + url);
+  return new Promise((resolve, reject) => {
+    http.get(`${url}${i}.html`, function (res, err) {
+      if (err) reject(err);
       
-      $('.co_content8 .ulink').each(function (idx, element) {
-        titles.push({
-          title: $(element).attr('href')
+      var chunks = [];
+      res.on('data', function (chunk) {
+        chunks.push(chunk);
+      }).on('end', function () {
+        var html = iconv.decode(Buffer.concat(chunks), 'gb2312');
+        var $ = cheerio.load(html, {decodeEntities: false});
+        
+        $('.co_content8 .ulink').each(function (idx, element) {
+          titles.push({
+            title: $(element).attr('href')
+          });
         });
+        if (i < 1) {
+          resolve(getMoveHomePage(url, ++index));
+        } else {
+          resolve(titles);
+        }
       });
-      if (i < 1) {
-        getTitle(url, ++index);
-      } else {
-        console.log(titles);
-        console.log('Title获取完毕！');
-        getBtLink(titles, count);
-      }
     });
   });
+  
 }
 
 function getBtLink (urls, n) {
-  console.log(`正在获取底${n}个url内容`);
-  console.log('http://www.ygdy8.net' + urls[n].title);
-  http.get('http://www.ygdy8.net' + urls[n].title, function (res) {
-    var chunks = [];
-    res.on('data', function (chunk) {
-      chunks.push(chunk);
-    }).on('end', function () {
-      var html = iconv.decode(Buffer.concat(chunks), 'gb2312');
-      var $ = cheerio.load(html, {decodeEntities: false});
+  console.log('正在获取' + urls[n].title);
+  return new Promise((resolve, reject) => {
+    http.get('http://www.ygdy8.net' + urls[n].title, function (res, err) {
+      if (err) reject(err);
       
-      $('#Zoom td').children('a').each(function (idx, element) {
-        btLink.push({bt: $(element).attr('href')});
+      var chunks = [];
+      res.on('data', function (chunk) {
+        chunks.push(chunk);
+      }).on('end', function () {
+        var html = iconv.decode(Buffer.concat(chunks), 'gb2312');
+        var $ = cheerio.load(html, {decodeEntities: false});
+        
+        $('#Zoom td').children('a').each(function (idx, element) {
+          btLink.push({bt: $(element).attr('href')});
+        });
+        
+        if (n < urls.length - 1) {
+          resolve(getBtLink(urls, ++count));
+        } else {
+          resolve(btLink);
+        }
       });
-      
-      if (n < urls.length - 1) {
-        getBtLink(urls, ++count);
-      } else {
-        console.log('btlink获取完毕');
-        console.log(btLink);
-        save(btLink);
-      }
-      
     });
-    
   });
 }
 
 const mongo_url = 'mongodb://localhost:27017';
 
 function save (btLink) {
-  MongoClient.connect(mongo_url, function (err, db) {
-    if (err) throw err;
-    console.log('数据库连接成功');
-    const myDb = db.db('dytt8-url');
-    const collection = myDb.collection('dytt8-url');
-    collection.insertMany(btLink, function (err, result) {
-      if (err) throw err;
-      console.log('数据保存成功');
+  return new Promise((resolve, reject) => {
+    MongoClient.connect(mongo_url, {useNewUrlParser: true}, function (err, db) {
+      if (err) reject(err);
+      console.log('数据库连接成功');
+      const myDb = db.db('dytt8-url');
+      const collection = myDb.collection('dytt8-url');
+      
+      collection.insertMany(btLink, function (err, result) {
+        if (err) reject(err);
+        resolve('数据保存成功');
+      });
+      db.close();
     });
-    db.close();
   });
 }
 
-function main () {
-  console.log('开始爬取数据');
-  getTitle(url, index);
+var index = 1;
+var count = 0;
+var titles = [];
+var btLink = [];
+const url = 'http://www.ygdy8.net/html/gndy/dyzz/list_23_';
+
+async function crawler () {
+  const titles = await getMoveHomePage(url, index);
+  const btLink = await getBtLink(titles, count);
+  return save(btLink);
 }
 
-main();
+crawler().then(result => {
+  console.log(result);
+});
+
